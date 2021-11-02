240d7ce8f1f07bb08b6dd0bc22f5ca755b5142bd
Jerome <josejeromelalunio@gmail.com>
2021-11-02 09:48:05 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studendInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i <= 31; i++)
+            {
+                this.comboBox1.Items.Add(i);
+            }
+            for (int i = 1; i <= 12; i++)
+            {
+                this.comboBox2.Items.Add(i);
+            }
+            for (int i = 1900; i <= 2021; i++)
+            {
+                this.comboBox3.Items.Add(i);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String name = textBox2.Text + " " + textBox3.Text + " " + textBox1.Text;
+            String gender = checkBox1.Checked ? "male" : checkBox2.Checked ? "female" : "didn't mention, probably gay or something";
+            String date = comboBox1.Text + "/" + comboBox2.Text + "/" + comboBox3.Text;
+            new Form2(name, gender, date).Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace studendInfo
+{
+    public partial class Form2 : Form
+    {
+        public Form2(String name, String gender, String date)
+        {
+            InitializeComponent();
+            label4.Text = name;
+            label5.Text = gender;
+            label6.Text = date;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
